e5f7f44f17542d74fde6da79e1b518c32c87af57
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2025-08-02 19:14:46 +0530

    feat: development done

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,11 +3,17 @@ import { client } from "@repo/db/client";
 export default async function Home() {
   const users = await client.user.findMany();
 
-  console.log(users);
-
   return (
-    <main>
-      <h1>Hello World</h1>
+    <main className="flex p-10 gap-10 flex-col items-center justify-center h-screen">
+      <h1>User App</h1>
+      {
+        users && users.map((user) => (
+          <div key={user.id}>
+            <h2>{user.username}</h2>
+            <p>{user.password}</p>
+          </div>
+        ))
+      }
     </main>
   );
 }
